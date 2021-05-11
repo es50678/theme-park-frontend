fb39fe4ac6e80bd4e08b86a3d6b1c01eabe314fa
Eduardo Hernandez Soto <es50678@gmail.com>
2021-05-10 20:43:57 -0400

    Module 2

--- src/config.js
+++ src/config.js
@@ -13,9 +13,9 @@ export const appConfig = {
   initStateAPI: 'https://tbdojckjrb.execute-api.us-east-1.amazonaws.com/Prod/InitState/',
   // MODULE 2 - REALTIME
   iot: {
-    poolId: '', // e.g. 'us-west-2:1abcdef-1234-abcd-1234-abcde123456'
-    host: '', // e.g. 'ab12ab12abcde.iot.us-east-1.amazonaws.com'
-    region: '' // e.g. 'us-west-1'
+    poolId: 'us-east-1:de285989-7b67-431a-9d89-ea1fecdab7b1', // e.g. 'us-west-2:1abcdef-1234-abcd-1234-abcde123456'
+    host: 'a30z42nuov05mm-ats.iot.us-east-1.amazonaws.com', // e.g. 'ab12ab12abcde.iot.us-east-1.amazonaws.com'
+    region: 'us-east-1' // e.g. 'us-west-1'
   },
   // MODULE 3 - PHOTOS
   photoUploadURL: '', // e.g. https://12abcdfg89.execute-api.us-west-2.amazonaws.com/Prod/Upload
